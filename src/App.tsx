1dd3195e24cdf42adb0f70383e2b12f2acd6cb56
Stephano Lioe <stephanolioe@gmail.com>
2020-08-16 21:50:39 +0200

    basic fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import { useQuery } from 'react-query'
 import './App.css';
 
+interface IUser {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  avatar: string;
+
+}
+
+const url = "https://reqres.in/api/users/"
+
+const fetchUsers = async () => {
+  const res = await fetch(url)
+  return res.json()
+}
+
 function App() {
+  const { data, status } = useQuery('getUsers', fetchUsers)
+  console.log(data, status)
+
   return (
     <div className="App">
-      react query playground, for crud
+      <h1>Users</h1>
+
+      { status === 'loading' && (
+        <div>Loading users...</div>
+      )}
+
+      { status === 'error' && (
+        <div>Error fetching the users</div>
+      )}
+
+      { status === 'success' && (
+        <div>
+          { data.data.map((user: IUser) => 
+            <div key={user.id}>{user.first_name} {user.last_name}</div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
